d7184e2f73a109376cd13f0144d8cbf3ae8fac89
전건우 <geonu1109@daou.co.kr>
2019-06-10 11:56:16 +0900

    add java 8 optional

--- FunctionalSwift/Java8.swift
+++ FunctionalSwift/Java8.swift
@@ -21,3 +21,49 @@ typealias BiFunction<T, U, R> = (T, U) -> R
 
 typealias Predicate<T> = (T) -> Bool
 typealias BiPredicate<T, U> = (T, U) -> Bool
+
+extension Optional {
+    static func empty() -> Optional<Wrapped> {
+        return nil
+    }
+    
+    static func of(_ value: Wrapped) -> Optional<Wrapped> {
+        return Optional<Wrapped>(value)
+    }
+    
+    var isPresent: Bool {
+        switch self {
+        case .some(_):
+            return true
+        case .none:
+            return false
+        }
+    }
+    
+    func ifPresent(_ consumer: Consumer<Wrapped>) {
+        switch self {
+        case .some(let value):
+            consumer(value)
+        case .none:
+            break
+        }
+    }
+    
+    func orElse(_ other: Wrapped) -> Wrapped {
+        switch self {
+        case .some(let value):
+            return value
+        case .none:
+            return other
+        }
+    }
+    
+    func orElseGet(_ supplier: Supplier<Wrapped>) -> Wrapped {
+        switch self {
+        case .some(let value):
+            return value
+        case .none:
+            return supplier()
+        }
+    }
+}
